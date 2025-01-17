5b57e9799c9590553849fa2b3e61bc1ed0104a28
izza-sirkast <dawlaizza405@gmail.com>
2025-01-17 23:34:58 +0700

    feat: handling edit and delete discussions, votes an answer, and delete an answer

--- routes/discussionRoutes.js
+++ routes/discussionRoutes.js
@@ -6,12 +6,14 @@ const router = express.Router();
 
 router.get("/get-all", DiscussionController.getAllDiscussions);
 router.get("/get-byid/:discussionId", DiscussionController.getDiscussionById);
+router.get("/get-hashtags/:discussionId", DiscussionController.getDiscussionHashtags);
 router.get("/get-discussion-author/:discussionId", DiscussionController.getDiscussionAuthor);
 router.get("/get-discussion-ud-author/:discussionId", authMiddleware, DiscussionController.getDiscussionUDAuthor);
 router.get("/get-questions", DiscussionController.getAllQuestions);
 router.get("/get-questions-ud", authMiddleware, DiscussionController.getQuestionsWithUD);
 router.get("/get-answers/:discussionId", DiscussionController.getAnswersByDiscussionId);
 router.get("/get-answers-user/:discussionId", DiscussionController.getAnswersWithItUser);
+router.get("/get-answers-user-ud/:discussionId", authMiddleware, DiscussionController.getAnswersUserUD);
 router.post("/create", authMiddleware, DiscussionController.createDiscussion);
 router.post("/create-hashtags", authMiddleware, DiscussionController.createDiscussionWithHashtags);
 router.post("/create-answer", authMiddleware, DiscussionController.createAnswer);
