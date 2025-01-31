a50a924765e069629ad1d8af4506e7cda0779bf7
ZulfiFazhar <zulfi.fazhar12@gmail.com>
2025-01-31 17:20:09 +0700

    feat: create delete route for imagekit uploading profile picture

--- controllers/imagekitController.js
+++ controllers/imagekitController.js
@@ -1,51 +1,73 @@
-const imagekit = require('../config/imagekit.config');
-const path = require('path');
-const uuid = require('uuid');
+const imagekit = require("../config/imagekit.config");
+const path = require("path");
+const uuid = require("uuid");
 
-class ImagekitController {  
-    static async uploadImage(req, res) {
-        if(req.file) {
-            const strFile = req.file.buffer.toString('base64');
-            const fileName = uuid.v4() + path.extname(req.file.originalname);
+class ImagekitController {
+  static async uploadImage(req, res) {
+    if (req.file) {
+      const strFile = req.file.buffer.toString("base64");
+      const fileName = uuid.v4() + path.extname(req.file.originalname);
 
-            // responseData = await imagekit.upload({
-            //     file : strFile,
-            //     fileName : fileName,
-            //     // checks: {file.size :  "1mb"}, // To run server side checks before uploading files. Notice the quotes around file.size and 1mb.
-            // }, function(error, result) {
-            //     if(error) console.log(error);
-            //     else console.log(result);
-            // });
-        
-            imagekit.upload({
-                file : strFile,
-                fileName : fileName
-            }).then(response => {
-                res.status(200).json({
-                    status: "success",
-                    message: "Image uploaded successfully",
-                    data : response,
-                });
-            }).catch(error => {
-                console.log(error);
-            });
-        }else{
-            res.status(400).json({
-                status: "error",
-                message: "Image not found",
-            });
-        }
+      // responseData = await imagekit.upload({
+      //     file : strFile,
+      //     fileName : fileName,
+      //     // checks: {file.size :  "1mb"}, // To run server side checks before uploading files. Notice the quotes around file.size and 1mb.
+      // }, function(error, result) {
+      //     if(error) console.log(error);
+      //     else console.log(result);
+      // });
 
-        // console.log(responseData);
-
-        // res.status(200).json({
-        //     status: "success",
-        //     message: "Image uploaded successfully",
-        //     data : responseData,
-        // });
-
+      imagekit
+        .upload({
+          file: strFile,
+          fileName: fileName,
+        })
+        .then((response) => {
+          res.status(200).json({
+            status: "success",
+            message: "Image uploaded successfully",
+            data: response,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      res.status(400).json({
+        status: "error",
+        message: "Image not found",
+      });
     }
-}
 
+    // console.log(responseData);
+
+    // res.status(200).json({
+    //     status: "success",
+    //     message: "Image uploaded successfully",
+    //     data : responseData,
+    // });
+  }
+
+  static async deleteImage(req, res) {
+    const fileId = req.params.fileId;
+
+    imagekit
+      .deleteFile(fileId)
+      .then((response) => {
+        res.status(200).json({
+          status: "success",
+          message: "Image deleted successfully",
+          data: response,
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          status: "error",
+          message: "Error deleting image",
+          error: error,
+        });
+      });
+  }
+}
 
 module.exports = ImagekitController;
